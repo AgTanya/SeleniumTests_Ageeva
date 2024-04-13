a51f72e6f895e04be3b4ab18a4a12c71f80bb404
Tatyana Ageeva <AgTanyaV@yandex.ru>
2024-04-13 18:45:25 +0500

    мой первый коммит

---
+++ SeleniumTests_Ageeva/SeleniumTestsForPractice.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests_Ageeva;
+
+public class SeleniumTestsForPractice
+{
+    [Test] //nUnit для запуска теста
+    public void Authorization()
+    {
+        var option = new ChromeOptions();
+        option.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
+        //Зайти в хром
+        var driver = new ChromeDriver(option);
+        //перейти по ссылке
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        //ждем 5 сек и затем переходим к поиску
+        Thread.Sleep(5000);
+        //вести логин и пароль
+        //находим элемент
+        var login = driver.FindElement(By.Id("Username"));
+        // ввод текста в стоку
+        login.SendKeys("agtanyav@yandex.ru");
+
+        var password = driver.FindElement(By.Id("Password"));
+        password.SendKeys("Lenovo2001!");
+
+        var btn = driver.FindElement(By.Name("button"));
+        btn.Click();
+        
+        Thread.Sleep(3000);
+        
+        //Проверка, что мы находимся на странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        
+        
+        //Закрыть браузер и процесс драйвера
+        driver.Quit();
+    }
+}
